6cc5ead81c473671c17a13b9489d98aa53015480
Song Yu <song.yu@marinerpartners.com>
2018-02-14 01:14:18 +0000

    d

--- monitoring_agent_test.py
+++ monitoring_agent_test.py
@@ -67,7 +67,7 @@ def service_start_monitoring_agent():
         #url = urlparse(target)
         kafka_broker = target
         logger.debug("kafka_broker:%s", kafka_broker)
-        producer = KafkaProducer(bootstrap_servers = kafka_broker) 
+        #producer = KafkaProducer(bootstrap_servers = kafka_broker) 
         atexit.register(shutdown_hook, producer)
         start_publish = True
         periodic_publish()
@@ -90,7 +90,7 @@ def publish_stats(enodeb_stats):
     try:
         for e in enodeb_stats:
             msg = 'enodeb' + ',' + str(e)   
-            producer.send(topic=produce_topic, key=str(e['enodeb']).encode('utf-8'), value=msg.encode('utf-8'))
+            #producer.send(topic=produce_topic, key=str(e['enodeb']).encode('utf-8'), value=msg.encode('utf-8'))
             logging.info('Publishing Enodeb event: %s', msg)
     except Exception as e:
             return e.__str__()
@@ -101,8 +101,32 @@ def periodic_publish():
         return
     logging.info('start periodic publish...')
     try:
-        r1 = requests.get(url = profile_URL)
-        r1_json = r1.json()
+        #r1 = requests.get(url = profile_URL)
+        #r1_json = r1.json()
+        r1_json = {
+      "ProfileArray" : [
+        {
+        "Name" : "Profile-1"  ,     
+        "DlSchedType"   : "RR",   
+        "DlAllocRBRate" : 70,     
+        "UlSchedType"   : "RR",        
+        "UlAllocRBRate" : 70  , 
+        "Start" : "15/05/2015 9:00",    
+        "End" :"15/05/2015 18:00" , 
+        "AdmControl" : 0 ,               
+        "CellIndividualOffset":0
+         },
+        {
+        "Name" : "Profile-2"  ,     
+        "DlSchedType"   : "RR",   
+        "DlAllocRBRate" : 70,     
+        "UlSchedType"   : "RR",        
+        "UlAllocRBRate" : 70  , 
+        "Start" : "15/05/2015 9:00",    
+        "End" :"15/05/2015 18:00" , 
+        "AdmControl" : 0 ,               
+        "CellIndividualOffset":0
+         }]}
         p_specs = stats.get_profile_specs(r1_json)
         logger.debug("p_specs:%s", str(p_specs))
         if len(p_specs):
@@ -114,10 +138,34 @@ def periodic_publish():
             """
             enodebs = [enodeb_demo]
             for enodeb in enodebs:
-                r3 = requests.get(url = enodeb_stats_url+'/'+enodeb+'/'+'1')
+                #r3 = requests.get(url = enodeb_stats_url+'/'+enodeb+'/'+'1')
                 logging.debug('calling %s to get enodeb stats',
                         enodeb_stats_url+'/'+enodeb+'/'+'1')
-                r3_json = r3.json()
+                #r3_json = r3.json()
+                r3_json = {
+                   "EnodeBStatsArray" :[
+                   {
+                    "Profile"  : "Profile-1",
+                    "StatsArray" :[
+                      {
+                        "Time"      :   time.time(),
+                        "DlBitrate" :   randint(0,40), 
+                        "UlBitrate" :   randint(0,40)
+                      }
+                     ]
+                    },
+                   {
+                     "Profile"  : "Profile-2",
+                     "StatsArray" :[
+                     {
+                       "Time"      :   time.time(),
+                       "DlBitrate" :   randint(0,40), 
+                       "UlBitrate" :   randint(0,40)
+                     }
+                     ]
+                    }
+                   ]
+                }
                 enodeb_stats = stats.get_enodeb_stats(r3_json, enodeb, p_specs)
                 logger.debug("enodeb stats:%s", str(enodeb_stats))
                 if len(enodeb_stats):
